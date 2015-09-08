fc3f75a61a9ddb09455a28297620bcf73bc6bf97
Victor <viclee02@gmail.com>
2015-09-07 23:00:46 -1000

    pass tests

--- vault.js
+++ vault.js
@@ -1,4 +1,22 @@
-'use strict';
+// 'use strict';
+// 
+// create a module that exposes 2 functions
 module.exports = function() {
-  
+
+  var secret = {};
+  return {
+    setValue : function (key, value){ //accepts two arguments key and value
+      secret[key] = value; //value in vault identified by it's key
+    },
+
+    getValue : function (key){
+      if (secret.hasOwnProperty(key)){ //determines whether an object has a property with the specified name, takes a string argument
+        return secret[key];
+      }else{
+        return null;
+      }
+    }
+  };
 };
+
+
